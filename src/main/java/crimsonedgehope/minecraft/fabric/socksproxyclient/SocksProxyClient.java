ccf299193a4d8856242a73e3380a7dfeb793695b
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-06-09 23:08:08 +0800

    Remove

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/SocksProxyClient.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/SocksProxyClient.java
@@ -1,12 +1,8 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient;
 
-import com.mojang.authlib.minecraft.UserApiService;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.ConfigUtils;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.mixin.MinecraftClientAccessor;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.HttpProxyServerUtils;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.HttpToSocksServer;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,9 +18,6 @@ public class SocksProxyClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		if (((MinecraftClientAccessor) MinecraftClient.getInstance()).getUserApiService().equals(UserApiService.OFFLINE)) {
-			LOGGER.warn("Offline mode! Now attempt to recreate Yggdrasil service");
-			HttpProxyServerUtils.recreateYggdrasilService();
-		}
+
 	}
 }
