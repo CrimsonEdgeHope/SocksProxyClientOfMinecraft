ac232ffecb43fee1b58293d4b119631745e8667c
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-06-10 16:09:40 +0800

    UPDATE

---
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/access/IAllowedAddressResolverMixin.java
@@ -0,0 +1,11 @@
+package crimsonedgehope.minecraft.fabric.socksproxyclient.access;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import org.xbill.DNS.DohResolver;
+
+@Environment(EnvType.CLIENT)
+public interface IAllowedAddressResolverMixin {
+    DohResolver socksProxyClient$getDohResolver();
+    void socksProxyClient$setDohResolver();
+}
