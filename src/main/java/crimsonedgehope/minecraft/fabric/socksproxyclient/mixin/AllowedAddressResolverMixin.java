12e0e396d8cdb091b35546e400a4c192d0eaf1d8
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-06-17 13:28:04 +0800

    UPDATE

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/mixin/AllowedAddressResolverMixin.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/mixin/AllowedAddressResolverMixin.java
@@ -11,8 +11,10 @@ import net.minecraft.client.network.AddressResolver;
 import net.minecraft.client.network.AllowedAddressResolver;
 import net.minecraft.client.network.RedirectResolver;
 import net.minecraft.client.network.ServerAddress;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -35,32 +37,41 @@ import java.util.Optional;
 @Environment(value= EnvType.CLIENT)
 @Mixin(AllowedAddressResolver.class)
 public class AllowedAddressResolverMixin implements IAllowedAddressResolverMixin {
-    @Shadow @Final private AddressResolver addressResolver;
-    @Shadow @Final private RedirectResolver redirectResolver;
+    @Shadow @Final @Mutable
+    private AddressResolver addressResolver;
+    @Shadow @Final @Mutable
+    private RedirectResolver redirectResolver;
 
     @Unique
-    private DohResolver resolver;
+    private DohResolver dohResolver;
 
     @Override
     public DohResolver socksProxyClient$getDohResolver() {
-        resolver.setUriTemplate(ServerConfig.minecraftRemoteResolveProviderUrl());
-        return resolver;
+        dohResolver.setUriTemplate(ServerConfig.minecraftRemoteResolveProviderUrl());
+        return dohResolver;
     }
 
     @Override
     public void socksProxyClient$setDohResolver() {
-        if (Objects.isNull(this.resolver)) {
-            this.resolver = new DohResolver(ServerConfig.minecraftRemoteResolveProviderUrl(), 2, Duration.ofSeconds(2L));
-            this.resolver.setUsePost(true);
+        if (Objects.isNull(this.dohResolver)) {
+            this.dohResolver = new DohResolver(ServerConfig.minecraftRemoteResolveProviderUrl(), 2, Duration.ofSeconds(2L));
+            this.dohResolver.setUsePost(true);
         }
     }
 
-    @Redirect(method = "resolve", at = @At(value = "FIELD", target = "Lnet/minecraft/client/network/AllowedAddressResolver;addressResolver:Lnet/minecraft/client/network/AddressResolver;"))
-    private AddressResolver redirectedGetAddressResolver(AllowedAddressResolver instance) {
-        if (!ServerConfig.minecraftRemoteResolve()) {
-            return addressResolver;
-        }
-        return serverAddress -> {
+    @Redirect(
+            method = "<init>",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/network/AllowedAddressResolver;addressResolver:Lnet/minecraft/client/network/AddressResolver;",
+                    opcode = Opcodes.PUTFIELD
+            )
+    )
+    private void redirectGetAddressResolver(AllowedAddressResolver instance, final AddressResolver originalAddressResolver) {
+        this.addressResolver = serverAddress -> {
+            if (!ServerConfig.minecraftRemoteResolve()) {
+                return originalAddressResolver.resolve(serverAddress);
+            }
             try {
                 if (InetAddresses.isInetAddress(serverAddress.getAddress())) {
                     InetAddress inet = InetAddress.getByName(serverAddress.getAddress());
@@ -68,7 +79,7 @@ public class AllowedAddressResolverMixin implements IAllowedAddressResolverMixin
                     // TODO: ?
                     return Optional.of(Address.create(new InetSocketAddress(InetAddress.getByAddress(inet.getHostAddress(), bytes), serverAddress.getPort())));
                 }
-                return addressResolve(serverAddress);
+                return addressResolver(serverAddress);
             } catch (Throwable e) {
                 SocksProxyClient.LOGGER.debug("Couldn't resolve server {} address", serverAddress.getAddress(), e);
             }
@@ -76,18 +87,25 @@ public class AllowedAddressResolverMixin implements IAllowedAddressResolverMixin
         };
     }
 
-    @Override
-    public RedirectResolver socksProxyClient$getRedirectResolver(AllowedAddressResolver instance) {
-        if (!ServerConfig.minecraftRemoteResolve()) {
-            return redirectResolver;
-        }
-        return serverAddress -> {
+    @Redirect(
+            method = "<init>",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/network/AllowedAddressResolver;redirectResolver:Lnet/minecraft/client/network/RedirectResolver;",
+                    opcode = Opcodes.PUTFIELD
+            )
+    )
+    private void redirectGetRedirectResolver(AllowedAddressResolver instance, final RedirectResolver originalRedirectResolver) {
+        this.redirectResolver = serverAddress -> {
+            if (!ServerConfig.minecraftRemoteResolve()) {
+                return originalRedirectResolver.lookupRedirect(serverAddress);
+            }
             if (serverAddress.getPort() == 25565) {
                 try {
                     if (InetAddresses.isInetAddress(serverAddress.getAddress())) {
                         return Optional.of(new ServerAddress(serverAddress.getAddress(), serverAddress.getPort()));
                     }
-                    return redirectResolve(serverAddress);
+                    return redirectResolver(serverAddress);
                 } catch (Throwable e) {
                     SocksProxyClient.LOGGER.debug("Couldn't resolve server {} address", serverAddress.getAddress(), e);
                 }
@@ -96,13 +114,8 @@ public class AllowedAddressResolverMixin implements IAllowedAddressResolverMixin
         };
     }
 
-    @Redirect(method = "resolve", at = @At(value = "FIELD", target = "Lnet/minecraft/client/network/AllowedAddressResolver;redirectResolver:Lnet/minecraft/client/network/RedirectResolver;"))
-    private RedirectResolver redirectedGetRedirectResolver(AllowedAddressResolver instance) {
-        return ((IAllowedAddressResolverMixin) instance).socksProxyClient$getRedirectResolver(instance);
-    }
-
     @Unique
-    private Record[] resolve(final String domainName, final int recordType) throws Exception {
+    private Record[] resolver(final String domainName, final int recordType) throws Exception {
         ((IAllowedAddressResolverMixin) this).socksProxyClient$setDohResolver();
         Lookup lookup;
         lookup = new Lookup(domainName, recordType);
@@ -121,8 +134,8 @@ public class AllowedAddressResolverMixin implements IAllowedAddressResolverMixin
     }
 
     @Unique
-    private Optional<Address> addressResolve(ServerAddress serverAddress) throws Exception {
-        Record[] records = resolve(serverAddress.getAddress(), Type.A);
+    private Optional<Address> addressResolver(ServerAddress serverAddress) throws Exception {
+        Record[] records = resolver(serverAddress.getAddress(), Type.A);
         final ARecord arec = (ARecord) records[0];
         SocksProxyClient.LOGGER.debug(arec.toString());
         InetAddress inetAddress = arec.getAddress();
@@ -133,9 +146,9 @@ public class AllowedAddressResolverMixin implements IAllowedAddressResolverMixin
     }
 
     @Unique
-    private Optional<ServerAddress> redirectResolve(ServerAddress serverAddress) throws Exception {
+    private Optional<ServerAddress> redirectResolver(ServerAddress serverAddress) throws Exception {
         String addr0 = "_minecraft._tcp." + serverAddress.getAddress();
-        Record[] records = resolve(addr0, Type.SRV);
+        Record[] records = resolver(addr0, Type.SRV);
         final SRVRecord srv = (SRVRecord) records[0];
         SocksProxyClient.LOGGER.debug(records[0].toString());
         String host = srv.getTarget().toString(true);
