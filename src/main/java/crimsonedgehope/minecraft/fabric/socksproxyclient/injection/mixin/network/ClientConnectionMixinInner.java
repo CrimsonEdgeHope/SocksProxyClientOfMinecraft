bfa60547920492d17d011820e70a99e09de38642
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-06-23 12:14:14 +0800

    UPDATE

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/network/ClientConnectionMixinInner.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/network/ClientConnectionMixinInner.java
@@ -2,25 +2,13 @@ package crimsonedgehope.minecraft.fabric.socksproxyclient.injection.mixin.networ
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.injection.access.IClientConnectionMixin;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.config.ServerConfig;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.ProxyCredential;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.ClientConnectionProxySelection;
 import io.netty.channel.ChannelPipeline;
-import io.netty.handler.proxy.Socks4ProxyHandler;
-import io.netty.handler.proxy.Socks5ProxyHandler;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.network.ClientConnection;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.SocketAddress;
-import java.util.Objects;
-
-import static crimsonedgehope.minecraft.fabric.socksproxyclient.SocksProxyClient.LOGGER;
 
 @Environment(EnvType.CLIENT)
 @Mixin(targets = "net.minecraft.network.ClientConnection$1")
@@ -28,44 +16,6 @@ public class ClientConnectionMixinInner {
     @WrapOperation(method = "initChannel", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;addFlowControlHandler(Lio/netty/channel/ChannelPipeline;)V"))
     private void wrapped(ClientConnection instance, ChannelPipeline pipeline, Operation<Void> original) {
         original.call(instance, pipeline);
-
-        InetSocketAddress remote = ((IClientConnectionMixin) instance).socksProxyClient$getInetSocketAddress();
-
-        if (Objects.isNull(remote)) {
-            return;
-        }
-
-        InetAddress address = remote.getAddress();
-
-        Proxy proxySelection;
-        if (address.isLoopbackAddress()) {
-            proxySelection = ServerConfig.getProxyForMinecraftLoopback();
-        } else {
-            proxySelection = ServerConfig.getProxyForMinecraft();
-        }
-
-        if (proxySelection.equals(Proxy.NO_PROXY)) {
-            LOGGER.info("No proxy on host {}", address);
-            return;
-        }
-
-        ProxyCredential proxyCredential = ServerConfig.getProxyCredential();
-
-        final SocketAddress sa = proxySelection.address();
-        switch (ServerConfig.getSocksVersion()) {
-            case SOCKS4:
-                LOGGER.info("Using Socks4 proxy {} on {}", sa, remote);
-                pipeline.addFirst("socks",
-                        new Socks4ProxyHandler(sa, proxyCredential.getUsername()));
-                break;
-            case SOCKS5:
-                LOGGER.info("Using Socks5 proxy {} on {}", sa, remote);
-                pipeline.addFirst("socks",
-                        new Socks5ProxyHandler(sa, proxyCredential.getUsername(), proxyCredential.getPassword()));
-                break;
-            default:
-                LOGGER.info("No proxy on host {}", address);
-                break;
-        }
+        ClientConnectionProxySelection.fire(instance, pipeline);
     }
 }
