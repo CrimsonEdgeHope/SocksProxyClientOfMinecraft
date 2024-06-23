1669101f30d092655ca2637ef36a719d3f053a34
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-06-23 13:32:55 +0800

    UPDATE

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/network/MultiplayerServerListPingerMixin.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/network/MultiplayerServerListPingerMixin.java
@@ -1,32 +1,40 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient.injection.mixin.network;
 
-import com.google.common.net.InetAddresses;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.config.ServerConfig;
-import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.DNSUtils;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.injection.mixin.netty.ChannelInitializerAccessor;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.ClientConnectionProxySelection;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import net.minecraft.client.network.MultiplayerServerListPinger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Environment(EnvType.CLIENT)
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 @Mixin(MultiplayerServerListPinger.class)
 public class MultiplayerServerListPingerMixin {
-    @ModifyArg(
-            method = "add",
+
+    @WrapOperation(method = "ping",
             at = @At(
                     value = "INVOKE",
-                    target = "Ljava/net/InetAddress;getByName(Ljava/lang/String;)Ljava/net/InetAddress;"
-            )
+                    target = "Lio/netty/bootstrap/Bootstrap;connect(Ljava/net/InetAddress;I)Lio/netty/channel/ChannelFuture;"
+            ),
+            remap = false
     )
-    private String modified(String host) throws Exception {
-        if (InetAddresses.isInetAddress(host)) {
-            return host;
-        }
-        if (ServerConfig.minecraftRemoteResolve()) {
-            return DNSUtils.resolveA(host).getHostAddress();
-        }
-        return host;
+    private ChannelFuture injected(Bootstrap instance, InetAddress inetHost, int inetPort, Operation<ChannelFuture> original) {
+        final InetSocketAddress remote = new InetSocketAddress(inetHost, inetPort);
+        final ChannelInitializer initializer = (ChannelInitializer) instance.config().handler();
+        instance.handler(new ChannelInitializer<>() {
+            @Override
+            protected void initChannel(Channel ch) throws Exception {
+                ((ChannelInitializerAccessor) initializer).invokeInitChannel(ch);
+                ClientConnectionProxySelection.fire(remote, ch.pipeline());
+            }
+        });
+        return original.call(instance, inetHost, inetPort);
     }
 }
