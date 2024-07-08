da597441602e3aeebd7e2ddfc73acd725f02bf26
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-07-08 18:10:50 +0800

    Minor patch

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/access/IClientConnectionMixin.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/access/IClientConnectionMixin.java
@@ -1,7 +1,11 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient.injection.access;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
 import java.net.InetSocketAddress;
 
+@Environment(EnvType.CLIENT)
 public interface IClientConnectionMixin {
     void socksProxyClient$setInetSocketAddress(InetSocketAddress inetSocketAddress);
     InetSocketAddress socksProxyClient$getInetSocketAddress();
