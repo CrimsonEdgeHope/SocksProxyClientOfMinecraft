699d5f1e16ba304db7a06c8988c368af7262abd6
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-07-09 14:37:50 +0800

    1.19.4

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/client/ServerResourcePackProviderMixin.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/client/ServerResourcePackProviderMixin.java
@@ -5,7 +5,7 @@ import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.HttpProxyServerUt
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.resource.ClientBuiltinResourcePackProvider;
+import net.minecraft.client.resource.ServerResourcePackProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.net.Proxy;
 
 @Environment(EnvType.CLIENT)
-@Mixin(ClientBuiltinResourcePackProvider.class)
-public class ClientBuiltinResourcePackProviderMixin {
+@Mixin(ServerResourcePackProvider.class)
+public class ServerResourcePackProviderMixin {
     @Redirect(method = "download", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getNetworkProxy()Ljava/net/Proxy;"))
     private Proxy redirectedGet(MinecraftClient instance) {
         return HttpProxyServerUtils.getProxyObject(ServerConfig.shouldProxyServerResourceDownload());
