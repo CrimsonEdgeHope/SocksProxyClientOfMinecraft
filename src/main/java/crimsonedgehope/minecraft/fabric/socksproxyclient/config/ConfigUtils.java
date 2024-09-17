b8d67baf71e8ef9cf3637683b53e8366a6d34f8a
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-09-17 11:51:15 +0800

    Refactor

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/ConfigUtils.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/ConfigUtils.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ConfigUtils {
 
-    public static <C extends SocksProxyClientConfig> C configInstance(final Class<C> clazz) throws Exception {
+    public static <C extends SocksProxyClientConfig> C getConfigInstance(final Class<C> clazz) throws Exception {
         Field field = clazz.getDeclaredField("INSTANCE");
         field.setAccessible(true);
         Object instance = field.get(null);
@@ -20,17 +20,17 @@ public final class ConfigUtils {
         return (C) instance;
     }
 
-    public static void loadAll() throws Exception {
-        configInstance(GeneralConfig.class).load();
-        configInstance(ServerConfig.class).load();
+    public static void loadAllConfig() throws Exception {
+        getConfigInstance(GeneralConfig.class).load();
+        getConfigInstance(ServerConfig.class).load();
     }
 
-    public static void saveAll() throws Exception {
-        configInstance(GeneralConfig.class).save();
-        configInstance(ServerConfig.class).save();
+    public static void saveAllConfig() throws Exception {
+        getConfigInstance(GeneralConfig.class).save();
+        getConfigInstance(ServerConfig.class).save();
     }
 
-    public static <T extends SocksProxyClientConfig> String categoryField(Class<T> clazz) throws Exception {
+    public static <T extends SocksProxyClientConfig> String getCategoryField(Class<T> clazz) throws Exception {
         String category = null;
         Field field = clazz.getDeclaredField("CATEGORY");
         field.setAccessible(true);
@@ -39,5 +39,15 @@ public final class ConfigUtils {
             category = clazz.getSimpleName();
         }
         return category;
+    }
+
+    public static SocksProxyClientConfigEntry<?> getEntryField(
+            final Class<? extends SocksProxyClientConfig> configClass, final String fieldName) throws Exception {
+        return getConfigInstance(configClass).getEntryField(fieldName);
+    }
+
+    public static <T> SocksProxyClientConfigEntry<T> getEntryField(
+            final Class<? extends SocksProxyClientConfig> configClass, final String fieldName, final Class<T> valueType) throws Exception {
+        return getConfigInstance(configClass).getEntryField(fieldName, valueType);
     }
 }
