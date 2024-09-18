4b8dc2fc60c83a9fb3929e7b03722aacbdf6e069
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-09-18 21:55:47 +0800

    YACL

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/GeneralConfig.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/GeneralConfig.java
@@ -2,9 +2,13 @@ package crimsonedgehope.minecraft.fabric.socksproxyclient.config;
 
 import com.google.gson.JsonObject;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.SocksProxyClient;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.i18n.TranslateKeys;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.ProxyCredential;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.SocksVersion;
 import lombok.Getter;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
@@ -12,6 +16,7 @@ import org.slf4j.Logger;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 
+@Environment(EnvType.CLIENT)
 public final class GeneralConfig extends SocksProxyClientConfig {
 
     private static final GeneralConfig INSTANCE;
@@ -25,19 +30,26 @@ public final class GeneralConfig extends SocksProxyClientConfig {
     public static final String CATEGORY = "general";
 
     private static final SocksProxyClientConfigEntry<Boolean> useProxy =
-            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "useProxy", false);
+            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "useProxy",
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_USEPROXY), false);
     private static final SocksProxyClientConfigEntry<SocksVersion> socksVersion =
-            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "socksVersion", SocksVersion.SOCKS5);
+            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "socksVersion",
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_SOCKSVERSION), SocksVersion.SOCKS5);
     private static final SocksProxyClientConfigEntry<String> customProxyHost =
-            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "customProxyHost", "localhost");
+            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "customProxyHost",
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_CUSTOMPROXYHOST), "localhost");
     private static final SocksProxyClientConfigEntry<Integer> customProxyPort =
-            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "customProxyPort", 1080);
+            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "customProxyPort",
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_CUSTOMPROXYPORT), 1080);
     private static final SocksProxyClientConfigEntry<String> customProxyUsername =
-            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "customProxyUsername", "");
+            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "customProxyUsername",
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_CUSTOMPROXYUSERNAME), "");
     private static final SocksProxyClientConfigEntry<String> customProxyPassword =
-            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "customProxyPassword", "");
+            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "customProxyPassword",
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_CUSTOMPROXYPASSWORD), "");
     private static final SocksProxyClientConfigEntry<Boolean> buttonsInMultiplayerScreen =
-            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "buttonsInMultiplayerScreen", true);
+            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "buttonsInMultiplayerScreen",
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_GENERAL_BUTTONSINMULTIPLAYERSCREEN), true);
 
     private GeneralConfig() {
         super(CATEGORY + ".json");
@@ -46,37 +58,37 @@ public final class GeneralConfig extends SocksProxyClientConfig {
     @Override
     public JsonObject defaultEntries() {
         JsonObject obj = new JsonObject();
-        obj.addProperty(useProxy.getEntry(), useProxy.getDefaultValue());
-        obj.addProperty(socksVersion.getEntry(), socksVersion.getDefaultValue().name());
-        obj.addProperty(customProxyHost.getEntry(), customProxyHost.getDefaultValue());
-        obj.addProperty(customProxyPort.getEntry(), customProxyPort.getDefaultValue());
-        obj.addProperty(customProxyUsername.getEntry(), customProxyUsername.getDefaultValue());
-        obj.addProperty(customProxyPassword.getEntry(), customProxyPassword.getDefaultValue());
-        obj.addProperty(buttonsInMultiplayerScreen.getEntry(), buttonsInMultiplayerScreen.getDefaultValue());
+        obj.addProperty(useProxy.getJsonEntry(), useProxy.getDefaultValue());
+        obj.addProperty(socksVersion.getJsonEntry(), socksVersion.getDefaultValue().name());
+        obj.addProperty(customProxyHost.getJsonEntry(), customProxyHost.getDefaultValue());
+        obj.addProperty(customProxyPort.getJsonEntry(), customProxyPort.getDefaultValue());
+        obj.addProperty(customProxyUsername.getJsonEntry(), customProxyUsername.getDefaultValue());
+        obj.addProperty(customProxyPassword.getJsonEntry(), customProxyPassword.getDefaultValue());
+        obj.addProperty(buttonsInMultiplayerScreen.getJsonEntry(), buttonsInMultiplayerScreen.getDefaultValue());
         return obj;
     }
 
     @Override
     public void fromJsonObject(JsonObject entries) {
-        useProxy.setValue(entries.get(useProxy.getEntry()).getAsBoolean());
-        socksVersion.setValue(SocksVersion.valueOf(entries.get(socksVersion.getEntry()).getAsString()));
-        customProxyHost.setValue(entries.get(customProxyHost.getEntry()).getAsString());
-        customProxyPort.setValue(entries.get(customProxyPort.getEntry()).getAsInt());
-        customProxyUsername.setValue(entries.get(customProxyUsername.getEntry()).getAsString());
-        customProxyPassword.setValue(entries.get(customProxyPassword.getEntry()).getAsString());
-        buttonsInMultiplayerScreen.setValue(entries.get(buttonsInMultiplayerScreen.getEntry()).getAsBoolean());
+        useProxy.setValue(entries.get(useProxy.getJsonEntry()).getAsBoolean());
+        socksVersion.setValue(SocksVersion.valueOf(entries.get(socksVersion.getJsonEntry()).getAsString()));
+        customProxyHost.setValue(entries.get(customProxyHost.getJsonEntry()).getAsString());
+        customProxyPort.setValue(entries.get(customProxyPort.getJsonEntry()).getAsInt());
+        customProxyUsername.setValue(entries.get(customProxyUsername.getJsonEntry()).getAsString());
+        customProxyPassword.setValue(entries.get(customProxyPassword.getJsonEntry()).getAsString());
+        buttonsInMultiplayerScreen.setValue(entries.get(buttonsInMultiplayerScreen.getJsonEntry()).getAsBoolean());
     }
 
     @Override
     public JsonObject toJsonObject() {
         JsonObject obj = new JsonObject();
-        obj.addProperty(useProxy.getEntry(), useProxy.getValue());
-        obj.addProperty(socksVersion.getEntry(), socksVersion.getValue().name());
-        obj.addProperty(customProxyHost.getEntry(), customProxyHost.getValue());
-        obj.addProperty(customProxyPort.getEntry(), customProxyPort.getValue());
-        obj.addProperty(customProxyUsername.getEntry(), customProxyUsername.getValue());
-        obj.addProperty(customProxyPassword.getEntry(), customProxyPassword.getValue());
-        obj.addProperty(buttonsInMultiplayerScreen.getEntry(), buttonsInMultiplayerScreen.getValue());
+        obj.addProperty(useProxy.getJsonEntry(), useProxy.getValue());
+        obj.addProperty(socksVersion.getJsonEntry(), socksVersion.getValue().name());
+        obj.addProperty(customProxyHost.getJsonEntry(), customProxyHost.getValue());
+        obj.addProperty(customProxyPort.getJsonEntry(), customProxyPort.getValue());
+        obj.addProperty(customProxyUsername.getJsonEntry(), customProxyUsername.getValue());
+        obj.addProperty(customProxyPassword.getJsonEntry(), customProxyPassword.getValue());
+        obj.addProperty(buttonsInMultiplayerScreen.getJsonEntry(), buttonsInMultiplayerScreen.getValue());
         return obj;
     }
 
---
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/YACLAccess.java
@@ -0,0 +1,38 @@
+package crimsonedgehope.minecraft.fabric.socksproxyclient.config.yacl;
+
+import dev.isxander.yacl3.api.YetAnotherConfigLib;
+import lombok.Getter;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+import java.util.Objects;
+
+@Environment(EnvType.CLIENT)
+@Getter
+final class YACLAccess {
+    private final Screen parentScreen;
+    private final YetAnotherConfigLib.Builder configBuilder;
+    private YetAnotherConfigLib yacl;
+    private Screen generatedScreen;
+
+    YACLAccess(Screen parentScreen, Text title) {
+        this.parentScreen = parentScreen;
+        this.configBuilder = YetAnotherConfigLib.createBuilder().title(title);
+    }
+
+    public YetAnotherConfigLib buildYacl() {
+        if (Objects.isNull(yacl)) {
+            yacl = configBuilder.build();
+        }
+        return yacl;
+    }
+
+    public Screen generateScreen() {
+        if (Objects.isNull(generatedScreen)) {
+            generatedScreen = buildYacl().generateScreen(parentScreen);
+        }
+        return generatedScreen;
+    }
+}
