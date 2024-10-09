b6c38e858321d49b8b70bcb78adac3aac224dbe5
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-10-09 16:55:32 +0800

    update

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/entry/ProxyEntry.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/entry/ProxyEntry.java
@@ -3,32 +3,34 @@ package crimsonedgehope.minecraft.fabric.socksproxyclient.config.entry;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.Credential;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.Socks;
 import lombok.Getter;
+import lombok.Setter;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.net.InetSocketAddress;
 import java.net.Proxy;
-import java.net.SocketAddress;
 
 @Environment(EnvType.CLIENT)
 @Getter
+@Setter
 public final class ProxyEntry {
-    private final Proxy proxy;
-    private final Socks version;
+    private Proxy proxy;
+    private Socks version;
 
     @NotNull
-    private final Credential credential;
+    private Credential credential;
 
-    public ProxyEntry(Socks version, SocketAddress sa) {
+    public ProxyEntry(Socks version, InetSocketAddress sa) {
         this(version, sa, null, null);
     }
 
-    public ProxyEntry(Socks version, SocketAddress sa, @Nullable String username, @Nullable String password) {
+    public ProxyEntry(Socks version, InetSocketAddress sa, @Nullable String username, @Nullable String password) {
         this(version, sa, new Credential(username, password));
     }
 
-    public ProxyEntry(Socks version, SocketAddress sa, @NotNull Credential credential) {
+    public ProxyEntry(Socks version, InetSocketAddress sa, @NotNull Credential credential) {
         this.proxy = new Proxy(Proxy.Type.SOCKS, sa);
         this.version = version;
         this.credential = credential;
