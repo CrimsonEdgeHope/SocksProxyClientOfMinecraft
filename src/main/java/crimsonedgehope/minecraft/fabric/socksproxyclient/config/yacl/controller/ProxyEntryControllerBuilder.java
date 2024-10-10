d6b8ea7d6d720b0659c645c9b587c2080a89279f
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-10-10 14:16:47 +0800

    Fix

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/controller/ProxyEntryControllerBuilder.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/controller/ProxyEntryControllerBuilder.java
@@ -4,13 +4,12 @@ import crimsonedgehope.minecraft.fabric.socksproxyclient.config.entry.ProxyEntry
 import dev.isxander.yacl3.api.Option;
 import dev.isxander.yacl3.api.controller.ControllerBuilder;
 import dev.isxander.yacl3.gui.YACLScreen;
-
-import java.util.function.BiConsumer;
+import org.apache.logging.log4j.util.TriConsumer;
 
 public interface ProxyEntryControllerBuilder extends ControllerBuilder<ProxyEntry> {
     static ProxyEntryControllerBuilderImpl create(Option<ProxyEntry> option) {
         return new ProxyEntryControllerBuilderImpl(option);
     }
 
-    ProxyEntryControllerBuilder action(BiConsumer<YACLScreen, ProxyEntry> action);
+    ProxyEntryControllerBuilder action(TriConsumer<YACLScreen, ProxyEntry, Runnable> action);
 }
