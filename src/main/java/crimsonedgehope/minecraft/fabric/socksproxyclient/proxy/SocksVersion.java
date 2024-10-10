7c2e87847aa8603b9261a48c18ac4fa19bbbea6a
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-10-10 12:05:35 +0800

    Fix

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/proxy/SocksVersion.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/proxy/SocksVersion.java
@@ -1,6 +1,11 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient.proxy;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum SocksVersion {
-    SOCKS4,
-    SOCKS5
+    SOCKS4("Socks 4", 4),
+    SOCKS5("Socks 5", 5);
+    public final String desc;
+    public final int ver;
 }
