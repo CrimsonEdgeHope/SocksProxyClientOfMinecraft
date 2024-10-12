c33b519a60e2e64788bfdd1fd3218418a0895e6a
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-10-12 13:37:02 +0800

    Remove the button in multiplayer screen

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/client/MinecraftClientMixin.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/client/MinecraftClientMixin.java
@@ -5,10 +5,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -17,14 +14,6 @@ import java.net.Proxy;
 @Environment(EnvType.CLIENT)
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-    @Mutable
-    @Shadow @Final private Proxy networkProxy;
-
-    @Redirect(method = "<init>", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;networkProxy:Ljava/net/Proxy;", opcode = Opcodes.PUTFIELD))
-    private void redirectedPut(MinecraftClient instance, Proxy value) {
-        this.networkProxy = HttpProxyUtils.getProxyObject();
-    }
-
     @Redirect(method = "<init>", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;networkProxy:Ljava/net/Proxy;", opcode = Opcodes.GETFIELD))
     private Proxy redirectedGet(MinecraftClient instance) {
         return HttpProxyUtils.getProxyObject();
