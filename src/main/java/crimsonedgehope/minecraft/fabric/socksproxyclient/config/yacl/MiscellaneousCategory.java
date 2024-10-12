95dcdcbdd833fef0beaded977faab01def8ea1ec
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-10-12 15:48:06 +0800

    Revert "Remove the button in multiplayer screen"
    This reverts commit bd235f9b73bfdbaf299dfcc0e5e872cb613e24ef.

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/MiscellaneousCategory.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/yacl/MiscellaneousCategory.java
@@ -1,11 +1,16 @@
 package crimsonedgehope.minecraft.fabric.socksproxyclient.config.yacl;
 
 import crimsonedgehope.minecraft.fabric.socksproxyclient.config.MiscellaneousConfig;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.config.entry.SocksProxyClientConfigEntry;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.i18n.TranslateKeys;
 import dev.isxander.yacl3.api.ConfigCategory;
+import dev.isxander.yacl3.api.Option;
+import dev.isxander.yacl3.api.controller.BooleanControllerBuilder;
 import net.minecraft.text.Text;
 
 final class MiscellaneousCategory extends YACLCategory<MiscellaneousConfig> {
+
+    SocksProxyClientConfigEntry<Boolean> buttonsInMultiplayerScreen;
 
     MiscellaneousCategory(YACLAccess yacl) {
         super(yacl, MiscellaneousConfig.class);
@@ -16,6 +21,14 @@ final class MiscellaneousCategory extends YACLCategory<MiscellaneousConfig> {
         ConfigCategory.Builder categoryBuilder = ConfigCategory.createBuilder();
 
         categoryBuilder.name(Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_MISCELLANEOUS));
+
+        buttonsInMultiplayerScreen = entryField("buttonsInMultiplayerScreen", Boolean.class);
+        Option<Boolean> yaclButtonsInMultiplayerScreen = Option.<Boolean>createBuilder()
+                .name(buttonsInMultiplayerScreen.getEntryTranslateKey())
+                .binding(buttonsInMultiplayerScreen.getDefaultValue(), buttonsInMultiplayerScreen::getValue, buttonsInMultiplayerScreen::setValue)
+                .controller(opt -> BooleanControllerBuilder.create(opt).yesNoFormatter().coloured(true))
+                .build();
+        categoryBuilder.option(yaclButtonsInMultiplayerScreen);
 
         return categoryBuilder.build();
     }
