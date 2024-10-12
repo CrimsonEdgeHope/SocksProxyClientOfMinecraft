22e39706da9b77d29aac4986a4a2370329296469
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-10-12 15:48:06 +0800

    Revert "Remove the button in multiplayer screen"
    This reverts commit bd235f9b73bfdbaf299dfcc0e5e872cb613e24ef.

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/MiscellaneousConfig.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/config/MiscellaneousConfig.java
@@ -2,8 +2,11 @@ package crimsonedgehope.minecraft.fabric.socksproxyclient.config;
 
 import com.google.gson.JsonObject;
 import crimsonedgehope.minecraft.fabric.socksproxyclient.SocksProxyClient;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.config.entry.SocksProxyClientConfigEntry;
+import crimsonedgehope.minecraft.fabric.socksproxyclient.i18n.TranslateKeys;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 
 @Environment(EnvType.CLIENT)
@@ -19,6 +22,11 @@ public final class MiscellaneousConfig extends SocksProxyClientConfig {
 
     public static final String CATEGORY = "miscellaneous";
 
+    private static final SocksProxyClientConfigEntry<Boolean> buttonsInMultiplayerScreen =
+            new SocksProxyClientConfigEntry<>(INSTANCE.getClass(), "buttonsInMultiplayerScreen",
+                    Text.translatable(TranslateKeys.SOCKSPROXYCLIENT_CONFIG_MISCELLANEOUS_BUTTONINMULTIPLAYERSCREEN),
+                    true);
+
     private MiscellaneousConfig() {
         super(CATEGORY + ".json");
     }
@@ -26,19 +34,23 @@ public final class MiscellaneousConfig extends SocksProxyClientConfig {
     @Override
     public JsonObject defaultEntries() {
         JsonObject obj = new JsonObject();
-
+        obj.addProperty(buttonsInMultiplayerScreen.getJsonEntry(), buttonsInMultiplayerScreen.getDefaultValue());
         return obj;
     }
 
     @Override
     public void fromJsonObject(JsonObject entries) {
-
+        buttonsInMultiplayerScreen.setValue(entries.get(buttonsInMultiplayerScreen.getJsonEntry()).getAsBoolean());
     }
 
     @Override
     public JsonObject toJsonObject() {
         JsonObject obj = new JsonObject();
-
+        obj.addProperty(buttonsInMultiplayerScreen.getJsonEntry(), buttonsInMultiplayerScreen.getValue());
         return obj;
+    }
+
+    public static boolean showButtonsInMultiplayerScreen() {
+        return buttonsInMultiplayerScreen.getValue();
     }
 }
