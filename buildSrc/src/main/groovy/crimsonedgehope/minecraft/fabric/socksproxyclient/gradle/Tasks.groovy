5e7ca1fc52543edd2945b683ea30f03853da1f76
CrimsonEdgeHope <92579614+CrimsonEdgeHope@users.noreply.github.com>
2024-10-12 15:46:01 +0800

    Fix gradle

--- buildSrc/src/main/groovy/crimsonedgehope/minecraft/fabric/socksproxyclient/gradle/Tasks.groovy
+++ buildSrc/src/main/groovy/crimsonedgehope/minecraft/fabric/socksproxyclient/gradle/Tasks.groovy
@@ -2,29 +2,15 @@ package crimsonedgehope.minecraft.fabric.socksproxyclient.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.BasePluginExtension
-import org.gradle.language.jvm.tasks.ProcessResources
 
-abstract class Tasks implements Plugin<Project> {
+// Had caused the whole mod to fail. Ridiculous. Root cause unclear.
+class Tasks implements Plugin<Project> {
     static final String GROUP = "SocksProxyClient"
 
     @Override
     void apply(Project project) {
         project.afterEvaluate {
-            project.tasks.register("projectGitHash", ProjectGitHash) {
-                it.doFirst {
-                    project.ext.compilation_java_version = project.targetCompatibility
-                }
-                it.doLast {
-                    def h = project.ext.git_short_hash
-                    println "Now on ${h}"
-                    def p = project.extensions.getByType(BasePluginExtension).archivesName
-                    p.set("${p.get()}-${h}")
-                }
-            }
-            project.tasks.named("processResources", ProcessResources) {
-                it.dependsOn(project.tasks.named("projectGitHash", ProjectGitHash))
-            }
+            project.tasks.register("projectGitHash", ProjectGitHash)
         }
     }
 }
