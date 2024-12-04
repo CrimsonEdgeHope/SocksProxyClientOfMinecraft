66ee7a3061db35ee962bb26bd3b3edfdfd72e844
NorthRealm <155140859+NorthRealm@users.noreply.github.com>
2024-12-04 17:14:47 +0800

    1.21.4

--- src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/client/MixinPlayerSkinTexture.java
+++ src/main/java/crimsonedgehope/minecraft/fabric/socksproxyclient/injection/mixin/client/MixinPlayerSkinTexture.java
@@ -5,7 +5,7 @@ import crimsonedgehope.minecraft.fabric.socksproxyclient.proxy.http.HttpProxyUti
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.texture.PlayerSkinTexture;
+import net.minecraft.client.texture.PlayerSkinTextureDownloader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,10 +13,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.net.Proxy;
 
 @Environment(EnvType.CLIENT)
-@Mixin(PlayerSkinTexture.class)
+@Mixin(PlayerSkinTextureDownloader.class)
 public class MixinPlayerSkinTexture {
-    @Redirect(method = "method_22801", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getNetworkProxy()Ljava/net/Proxy;"))
-    private Proxy redirectedGet(MinecraftClient instance) {
+    @Redirect(method = "download", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getNetworkProxy()Ljava/net/Proxy;"))
+    private static Proxy redirectedGet(MinecraftClient instance) {
         return HttpProxyUtils.getProxyObject(ServerConfig.shouldProxyPlayerSkinDownload());
     }
 }
